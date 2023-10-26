85a3f4bc1310ff233d37d1ae0f2ffba5a0739eee
omerelias <omerelias123456@gmail.com>
2023-10-26 19:09:20 +0300

    Get and Push data with axios

--- lesson3/myapp/src/get_post_data/updateuser.jsx
+++ lesson3/myapp/src/get_post_data/updateuser.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
-const Updateuser = () => {
-    return (
-        <div>
-             test One aDDDDssv`aa  
-        </div>
-    );
-}
+const usersUrl = 'https://jsonplaceholder.typicode.com/users';
 
-export default Updateuser;
+const UpdateUserComp = () => {
+  const [userId, setUserId] = useState('');
+  const [user, setUser] = useState({ name: '', email: '' });
+
+  const getData = async () => {
+    const { data } = await axios.get(`${usersUrl}/${userId}`);
+    setUser({ name: data.name, email: data.email });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.patch(`${usersUrl}/${userId}`, user);
+    console.log(data);
+  };
+
+  return (
+    <>
+      User ID:{' '}
+      <input type='number' onInput={(e) => setUserId(e.target.value)} />
+      <button onClick={getData}>Get Data</button>
+      <br /> <br />
+      <form onSubmit={handleSubmit}>
+        Name:{' '}
+        <input
+          type='text'
+          value={user.name}
+          onInput={(e) => setUser({ ...user, name: e.target.value })}
+        />
+        <br />
+        Email:{' '}
+        <input
+          type='email'
+          value={user.email}
+          onInput={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <br />
+        <button type='submit'>Update</button>
+      </form>
+    </>
+  );
+};
+
+
+export default UpdateUserComp;
